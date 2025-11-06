069bc6aaab55aade49d83e31f37162f02fa09ba8
Christopher Ohara <cohara@g.ecc.u-tokyo.ac.jp>
2025-11-06 09:42:19 +0900

    Delete sql/crud/q2.sql

--- sql/crud/q2.sql
+++
@@ -1,8 +0,0 @@
-WITH ins AS (
-  INSERT INTO job_title(job_title_nm)
-  VALUES ('Web Programmer')
-  ON CONFLICT (job_title_nm) DO UPDATE
-    SET job_title_nm = EXCLUDED.job_title_nm
-  RETURNING job_title_id, job_title_nm
-)
-SELECT * FROM ins;
